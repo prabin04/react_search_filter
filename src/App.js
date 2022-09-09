aa13924ea8185ab551bf0f9e9ebd5e405306f07e
prabin04 <prabin.b9@gmail.com>
2022-09-09 12:47:52 +0200

    Basic Search func

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import React, {useState} from 'react'
 import './App.css';
 import {Users} from "./users"
 
 function App() {
+  const [query, setQuery] = useState("")
+  console.log(Users.filter(user=>user.first_name.toLowerCase().includes("fe")));
   return (
     <div className="app">
-      <input type="text" placeholder='search' className="search" />
+      <input
+        type="text"
+        placeholder='search'
+        className="search"
+        onChange={e=>setQuery(e.target.value)} />
       <ul className="list">
-        {Users.map((user) => (
+        {Users.filter(user=>user.first_name.toLocaleLowerCase().includes(query)).map((user) => (
           <li key={user.id} className="listItem">{user.first_name}</li>
         ))}
       </ul>
